9111ef2106cbb1092a93cc874b378853587e158c
mast <devnull@localhost>
2018-12-18 15:46:18 -0700

    Changes for extra apertures

--- Shared/SharedJeolDefines.h
+++ Shared/SharedJeolDefines.h
@@ -36,6 +36,7 @@
 enum {JEOL_MDS_OFF = 0, JEOL_MDS_SEARCH, JEOL_MDS_FOCUS, JEOL_MDS_PHOTOSET};
 
 #define JEOL_HAS_NITROGEN_CLASS (1)
+#define JEOL_HAS_EXTRA_APERTURES  (1 << 1)
 
 // Standard conversions from signed real to nearest integer for JEOL calls
 #define NINT8000(a) (long)floor((a) + 0x8000 + 0.5)
