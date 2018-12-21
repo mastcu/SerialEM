f9b9fa1b2e702b9007de60b907aa6d49e6448148
mast <devnull@localhost>
2018-12-20 22:51:39 -0700

    32-bit SerialEM.lib was getting cleaned by 64-bit build, so do 64 bit first

--- buildAll.bat
+++ buildAll.bat
@@ -4,13 +4,14 @@ set PATH=c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\BIN\amd64;C:\win
 set LIB=c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\LIB\amd64;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\LIB\amd64;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\lib\x64
 set INCLUDE=c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\INCLUDE;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\include
 
+msbuild /t:Rebuild /p:Configuration=Release /p:Platform=x64 SerialEM.vcxproj
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 msbuild /t:Rebuild /p:Configuration=NoHang /p:Platform=Win32 SerialEM.vcxproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 msbuild /t:Rebuild /p:Configuration=Release /p:Platform=Win32 SerialEM.vcxproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild /t:Rebuild /p:Configuration=Release /p:Platform=x64 SerialEM.vcxproj
-if %errorlevel% neq 0 exit /b %errorlevel%
 
 
