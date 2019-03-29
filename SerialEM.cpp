72c7285a408e11acd5e58fc8d18796d0a1524b49
mast <devnull@localhost>
2019-03-29 08:01:58 -0600

    For new plugin function and new version 3.7 merge

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -69,7 +69,7 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 #define VERSION_STRING  "SerialEM Version 3.8.0 beta"
-#define TAG_STRING      "(Tagged SEM_3-7-2, 3/6/19)"
+#define TAG_STRING      "(Tagged SEM_3-7-3, 3/23/19)"
 
 // Offsets for static window inside main frame
 #define STATIC_BORDER_TOP      0
