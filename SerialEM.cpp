eba2003c2205a93f336b7f255a99e8731e101408
mast <devnull@localhost>
2019-05-28 17:04:26 -0600

    version Added tag SEM_3-7-5 for changeset ed2356bc26c3 3.7 merge

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -69,7 +69,7 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 #define VERSION_STRING  "SerialEM Version 3.8.0 beta"
-#define TAG_STRING      "(Tagged SEM_3-7-4, 4/17/19)"
+#define TAG_STRING      "(Tagged SEM_3-7-5, 5/22/19)"
 
 // Offsets for static window inside main frame
 #define STATIC_BORDER_TOP      0
