aaef2776883f347ca40bdbf856efd3aef6637ec9
mast <devnull@localhost>
2021-04-03 13:31:42 -0600

    3.8 merge

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -73,7 +73,7 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 #define VERSION_STRING  "SerialEM Version 3.9.0beta"
-#define TAG_STRING      "(Tagged SEM_3-8-7, 12/31/20)"
+#define TAG_STRING      "(Tagged SEM_3-8-8, 4/2/21)"
 
 // Offsets for static window inside main frame
 #define STATIC_BORDER_TOP      0
