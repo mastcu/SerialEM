35b2160143fdb755a9c67b749c66b50f367659f9
mast <devnull@localhost>
2022-05-18 12:18:56 +0100

    Add recursive mkdir and  make script use it

--- MacroCommands.cpp
+++ MacroCommands.cpp
@@ -3500,6 +3500,7 @@ int CMacCmd::AllowFileOverwrite(void)
 int CMacCmd::SetDirectory(void)
 {
   CFileStatus status;
+  CString mess;
 
   SubstituteLineStripItems(mStrLine, 1, mStrCopy);
   if (mStrCopy.IsEmpty())
@@ -3511,10 +3512,11 @@ int CMacCmd::SetDirectory(void)
     if (CFile::GetStatus((LPCTSTR)mStrCopy, status)) {
       mWinApp->AppendToLog("Not making directory " + mStrCopy + " - it already exists");
     } else {
-      if (!_mkdir((LPCTSTR)mStrCopy))
+      if (!UtilRecursiveMakeDir(mStrCopy, mess))
         mWinApp->AppendToLog("Created directory " + mStrCopy);
       else
-        SUSPEND_NOLINE("because of failure to create directory " + mStrCopy);
+        SUSPEND_NOLINE("because of failure to create directory " + mStrCopy + ":\n" + 
+          mess);
     }
   }
   return 0;
