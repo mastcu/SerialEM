3e8432f11762fb914002b46e7b3cae6720e711e4
mast <devnull@localhost>
2022-12-15 02:59:22 +0000

    version

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -75,8 +75,8 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#define VERSION_STRING  "SerialEM Version 4.0.15"
-#define TAG_STRING      "(Tagged SEM_4-0-15, 11/17/22)"
+#define VERSION_STRING  "SerialEM Version 4.0.16"
+#define TAG_STRING      "(Tagged SEM_4-0-16, 12/14/22)"
 
 // Offsets for static window inside main frame
 #define STATIC_BORDER_TOP      0
