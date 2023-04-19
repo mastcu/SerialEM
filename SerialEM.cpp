3e68df2426672937566b9554c044893f9fba6858
mast <devnull@localhost>
2023-04-19 20:54:07 +0100

    *** empty log message ***

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -75,8 +75,8 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#define VERSION_STRING  "SerialEM Version 4.0.21"
-#define TAG_STRING      "(Tagged SEM_4-0-21, 4/18/23)"
+#define VERSION_STRING  "SerialEM Version 4.0.22"
+#define TAG_STRING      "(Tagged SEM_4-0-22, 4/19/23)"
 
 // Offsets for static window inside main frame
 #define STATIC_BORDER_TOP      0
