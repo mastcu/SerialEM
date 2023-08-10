95b21a5a544c301d37e95780b2545cae2d99a023
mast <devnull@localhost>
2023-08-10 16:35:32 +0100

    version and K3 fix and basic mode fix

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -75,8 +75,8 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#define VERSION_STRING  "SerialEM Version 4.0.26"
-#define TAG_STRING      "(Tagged SEM_4-0-26, 6/27/23)"
+#define VERSION_STRING  "SerialEM Version 4.0.27"
+#define TAG_STRING      "(Tagged SEM_4-0-27, 8/10/23)"
 
 // Offsets for static window inside main frame
 #define STATIC_BORDER_TOP      0
