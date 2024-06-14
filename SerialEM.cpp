c3e6a16a391b5065565b28a8a5bd057c21398045
mast <devnull@localhost>
2024-06-14 17:16:26 +0100

    Tietz scan fixes

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -461,7 +461,7 @@ CSerialEMApp::CSerialEMApp()
       mCamConSets[i][k].integration = 1;
       mCamConSets[i][k].correctDrift = -1;
     }
-    mCamParams[i].minPixelTime = 0.1f;
+    mCamParams[i].minPixelTime = -0.1f;
     mCamParams[i].maxPixelTime = 0.;
     mCamParams[i].pixelTimeIncrement = 0.;
     mCamParams[i].maxIntegration = 0;
