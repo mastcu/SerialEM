032106bc62db98bb9f6e9b9b10b73126a0f20d70
mast <devnull@localhost>
2024-09-26 19:28:40 +0100

    Fix HQ in LD and version

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -77,8 +77,8 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#define VERSION_STRING  "SerialEM Version 4.1.16"
-#define TAG_STRING      "(Tagged SEM_4-1-16, 9/15/24)"
+#define VERSION_STRING  "SerialEM Version 4.1.17"
+#define TAG_STRING      "(Tagged SEM_4-1-17, 9/26/24)"
 #define DEPRECATED_PYTHON  "3.5-64"
 
 // Offsets for static window inside main frame
