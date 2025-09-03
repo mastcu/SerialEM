e24290d6bda2c0e1db38bd7e21dd0f17d3234efb
mast <mast@colorado.edu>
2025-09-03 13:07:47 -0600

    doc and version
    --HG--
    branch : SEM_4-2

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -80,8 +80,8 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#define VERSION_STRING  "SerialEM Version 4.2.7"
-#define TAG_STRING      "(Tagged SEM_4-2-7, 8/7/25)"
+#define VERSION_STRING  "SerialEM Version 4.2.8"
+#define TAG_STRING      "(Tagged SEM_4-2-8, 9/3/25)"
 #define DEPRECATED_PYTHON  "3.6-64"
 
 // Offsets for static window inside main frame
