1eb7451616d6c640b19d8d555226aabf45c9641e
mast <mast@colorado.edu>
2025-10-03 17:29:10 -0600

    fix for screen hang
    --HG--
    branch : SEM_4-2

--- EMscope.cpp
+++ EMscope.cpp
@@ -3808,6 +3808,7 @@ BOOL CEMscope::SynchronousScreenPos(int inPos)
     SleepMsg(50);
   }
   SEMMessageBox("Timeout setting screen position");
+  UtilThreadCleanup(&mScreenThread);
   return false;
 }
 
