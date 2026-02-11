db5e7ef29c2bf4cc05f99d62894b1a8a5bca24ed
mast <mast@colorado.edu>
2026-02-11 14:33:03 -0700

    fix edit focus crash

--- NavigatorDlg.cpp
+++ NavigatorDlg.cpp
@@ -1322,7 +1322,8 @@ void CNavigatorDlg::AddFocusAreaPoint(bool drawFirst)
     if (drawFirst)
       Redraw();
     imBuf->mHasUserPt = true;
-    mLowDoseDlg->FixUserPoint(imBuf, 0);
+    if (imBuf->mImage)
+      mLowDoseDlg->FixUserPoint(imBuf, 0);
   }
 }
 
