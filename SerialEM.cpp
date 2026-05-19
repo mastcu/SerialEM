7a837871d03bc07dbd62c9d4a05141a28e2e7a52
mast <mast@colorado.edu>
2026-05-19 10:25:39 -0600

    version

--- SerialEM.cpp
+++ SerialEM.cpp
@@ -80,8 +80,8 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#define VERSION_STRING  "SerialEM Version 4.2.22"
-#define TAG_STRING      "(Tagged SEM_4-2-22, 5/7/26)"
+#define VERSION_STRING  "SerialEM Version 4.2.23"
+#define TAG_STRING      "(Tagged SEM_4-2-23, 5/19/26)"
 #define DEPRECATED_PYTHON  "3.6-64"
 
 // Offsets for static window inside main frame
