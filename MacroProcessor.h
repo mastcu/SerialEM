64fa87c4cc7b6a31ee44081ab80dd667f18c88e8
leo <lcrowder101@gmail.com>
2026-08-05 15:07:58 -0700

    minor fixes, formatting, names

--- MacroProcessor.h
+++ MacroProcessor.h
@@ -557,7 +557,6 @@ protected:
 public:
   void SetNumCamMacRows(int inVal);
   void SetNumStatusLines(int inVal);
-  int SetStatus(int lineNum, CString text);
   void GetNextLine(CString * macro, int & currentIndex, CString &strLine, bool commentOK = false);
   int ScanForName(int macroNumber, CString *macro = NULL);
   COLORREF TranslateColorEntry(CString *strItems);
